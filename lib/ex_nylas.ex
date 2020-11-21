b2999dd676a3c48505ef6f9dce9f832bd6348216
nicholasbair <bairinc@gmail.com>
2020-11-20 21:08:00 -0700

    Init

---
+++ lib/ex_nylas.ex
@@ -0,0 +1,18 @@
+defmodule ExNylas do
+  @moduledoc """
+  Documentation for `ExNylas`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExNylas.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ex_nylas_test.exs
@@ -0,0 +1,8 @@
+defmodule ExNylasTest do
+  use ExUnit.Case
+  doctest ExNylas
+
+  test "greets the world" do
+    assert ExNylas.hello() == :world
+  end
+end
