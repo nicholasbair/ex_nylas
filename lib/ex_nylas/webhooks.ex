13d498d54094fe095fe4fb647ef78ae019a3ef99
nickbair <bairinc@gmail.com>
2023-03-24 12:39:30 -0600

    Add webhook verification

--- lib/ex_nylas/webhooks.ex
+++ lib/ex_nylas/webhooks.ex
@@ -26,6 +26,7 @@ defmodule ExNylas.Webhooks do
   @moduledoc """
   Interface for Nylas webhook.
   """
+  alias ExNylas.Connection, as: Conn
 
   use ExNylas,
     object: "webhooks",
@@ -33,4 +34,27 @@ defmodule ExNylas.Webhooks do
     header_type: :header_basic,
     use_client_url: true,
     include: [:list, :first, :find, :delete, :build, :update, :create, :all]
+
+  @doc """
+  Validate the X-Nylas-Signature header from a webhook.
+
+  Example
+      valid = conn |> ExNylas.Webhoooks.valid_signature?(body, signature_from_webhook_request)
+  """
+  def valid_signature?(%Conn{client_secret: secret}, _body, _signature) when is_nil(secret) do
+    raise "ExNylas.Connection struct is missing a value for `client_secret` which is required for this operation."
+  end
+
+  def valid_signature?(%Conn{} = _conn, body, _signature) when not is_bitstring(body) do
+    raise "body should be passed as a string."
+  end
+
+  def valid_signature?(%Conn{} = conn, body, signature) do
+    computed =
+      :crypto.mac(:hmac, :sha256, conn.client_secret, body)
+      |> Base.encode16
+      |> String.downcase()
+
+    computed == String.downcase(signature)
+  end
 end
