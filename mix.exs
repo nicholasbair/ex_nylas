953d06a751a398b57574507e1cd0bad5188bff92
nickbair <bairinc@gmail.com>
2023-12-21 19:00:09 -0700

    Remove unused dep

--- mix.exs
+++ mix.exs
@@ -37,7 +37,6 @@ defmodule ExNylas.MixProject do
       {:req, "~> 0.4.8"},
       {:poison, "~> 5.0"},
       {:multipart, "~> 0.4.0"},
-      {:bypass, "~> 2.1", only: :test},
       {:typed_struct, "~> 0.3.0"},
       {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
       {:dialyxir, "~> 1.4", only: [:dev, :test], runtime: false}
