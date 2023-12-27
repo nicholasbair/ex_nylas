8bfcdd60a0bba6b83d84900a049b906e23c1aad3
nickbair <bairinc@gmail.com>
2023-12-27 16:30:15 -0700

    Add HTTP tests

---
+++ test/util_test/http_test.exs
@@ -0,0 +1,76 @@
+defmodule ExNylasTest.HTTP do
+  use ExUnit.Case, async: true
+
+  setup do
+    bypass = Bypass.open()
+    {:ok, bypass: bypass}
+  end
+
+  test "client can handle an unsuccessful request", %{bypass: bypass} do
+    Bypass.expect_once(bypass, "GET", "/v3/grants/1234/messages", fn conn ->
+      Plug.Conn.resp(conn, 429, ~s<[]>)
+    end)
+
+    res =
+      ExNylas.Messages.list(
+        %ExNylas.Connection{
+          grant_id: "1234",
+          api_key: "1234",
+          api_server: endpoint_url(bypass.port),
+          options: [retry: false]
+        }
+      )
+
+    assert match?({:error, _}, res)
+  end
+
+  test "client can recover from server downtime", %{bypass: bypass} do
+    Bypass.expect(bypass, fn conn ->
+      # We don't care about `request_path` or `method` for this test.
+      Plug.Conn.resp(conn, 200, ~s<[]>)
+    end)
+
+    res =
+      ExNylas.Messages.list(
+        %ExNylas.Connection{
+          grant_id: "1234",
+          api_key: "1234",
+          api_server: endpoint_url(bypass.port),
+          options: [retry: false]
+        }
+      )
+
+    assert match?({:ok, _}, res)
+
+    # Blocks until the TCP socket is closed.
+    Bypass.down(bypass)
+
+    res =
+      ExNylas.Messages.list(
+        %ExNylas.Connection{
+          grant_id: "1234",
+          api_key: "1234",
+          api_server: endpoint_url(bypass.port),
+          options: [retry: false]
+        }
+      )
+
+    assert match?({:error, _}, res)
+
+    Bypass.up(bypass)
+
+    res =
+      ExNylas.Messages.list(
+        %ExNylas.Connection{
+          grant_id: "1234",
+          api_key: "1234",
+          api_server: endpoint_url(bypass.port),
+          options: [retry: false]
+        }
+      )
+
+    assert match?({:ok, _}, res)
+  end
+
+  defp endpoint_url(port), do: "http://localhost:#{port}/"
+end
