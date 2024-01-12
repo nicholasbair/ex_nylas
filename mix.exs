98d1f4fd47418e7dd5453f111485ffe336d2c7f2
nickbair <bairinc@gmail.com>
2024-01-12 09:24:30 -0700

    Release

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ExNylas.MixProject do
   def project do
     [
       app: :ex_nylas,
-      version: "0.3.1",
+      version: "0.3.2",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
