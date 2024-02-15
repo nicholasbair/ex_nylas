e56cfb5163c46d0f0398c962231f765cf69d9a3f
nickbair <bairinc@gmail.com>
2024-02-15 15:59:15 -0700

    Bump version in mix file

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ExNylas.MixProject do
   def project do
     [
       app: :ex_nylas,
-      version: "0.3.3",
+      version: "0.3.4",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
