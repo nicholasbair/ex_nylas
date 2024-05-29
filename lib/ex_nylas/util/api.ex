113ee0b2941d586dd5c0623f81f555e26b8d18a0
nickbair <bairinc@gmail.com>
2024-05-29 16:51:00 -0600

    Remove alias

--- lib/ex_nylas/util/api.ex
+++ lib/ex_nylas/util/api.ex
@@ -1,7 +1,6 @@
 defmodule ExNylas.API do
   @moduledoc false
 
-  alias Plug.Exception
   alias ExNylas.Connection, as: Conn
   alias ExNylas.Transform, as: TF
 
@@ -77,7 +76,7 @@ defmodule ExNylas.API do
 
   # Responses ###################################################################
 
-  @spec handle_response({atom(), Req.Response.t() | Exception.t()}, any(), boolean()) :: {:ok, any()} | {:error, any()}
+  @spec handle_response({atom(), Req.Response.t() | map()}, any(), boolean()) :: {:ok, any()} | {:error, any()}
   def handle_response(res, transform_to \\ nil, use_common_response \\ true) do
     case format_response(res) do
       {:ok, body, status} ->
