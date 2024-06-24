2549b0436a74b06b447985725fc6f4a20ecbd67a
nickbair <bairinc@gmail.com>
2024-06-24 16:20:33 -0600

    Add pub/sub channels

--- lib/ex_nylas/channels/build.ex
+++ lib/ex_nylas/channels/build.ex
@@ -1,25 +1,26 @@
-defmodule ExNylas.Webhook.Build do
+defmodule ExNylas.Channel.Build do
   @moduledoc """
-  Helper module for validating a webhook before creating/updating it.
+  Helper module for validating a channel before creating/updating it.
   """
 
   use TypedEctoSchema
   import Ecto.Changeset
 
-  @derive {Jason.Encoder, only: [:description, :trigger_types, :webhook_url, :notification_email_addresses]}
+  @derive {Jason.Encoder, only: [:description, :trigger_types, :notification_email_addresses, :topic, :encryption_key]}
   @primary_key false
 
   typed_embedded_schema do
     field(:description, :string)
     field(:notification_email_addresses, {:array, :string})
+    field(:encryption_key, :string)
+    field(:topic, :string)
     field(:trigger_types, {:array, :string}, null: false)
-    field(:webhook_url, :string, null: false)
   end
 
   @doc false
   def changeset(struct, params \\ %{}) do
     struct
     |> cast(params, __MODULE__.__schema__(:fields))
-    |> validate_required([:trigger_types, :webhook_url])
+    |> validate_required([:trigger_types, :topic])
   end
 end
