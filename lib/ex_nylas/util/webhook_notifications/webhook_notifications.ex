59854f5880d1477877f44bf3fa8c373f9dabcd92
Nick Bair <bairinc@gmail.com>
2024-07-19 09:46:40 -0600

    Rename valid_signature -> validate_signature (#257)
    * Rename valid_signature -> validate_signature
    
    * Bump timeout in GH action
    
    * Update tests

--- lib/ex_nylas/util/webhook_notifications/webhook_notifications.ex
+++ lib/ex_nylas/util/webhook_notifications/webhook_notifications.ex
@@ -40,22 +40,22 @@ defmodule ExNylas.WebhookNotifications do
 
   ## Examples
 
-      iex> {:ok, match?} = ExNylas.WebhoookNotification.valid_signature(webhook_secret, body, signature_from_webhook_request)
+      iex> {:ok, match?} = ExNylas.WebhoookNotification.validate_signature(webhook_secret, body, signature_from_webhook_request)
   """
-  @spec valid_signature(String.t(), String.t(), String.t()) :: {:ok, boolean()} | {:error, String.t()}
-  def valid_signature(webhook_secret, _body, _signature) when is_nil(webhook_secret) do
+  @spec validate_signature(String.t(), String.t(), String.t()) :: {:ok, boolean()} | {:error, String.t()}
+  def validate_signature(webhook_secret, _body, _signature) when is_nil(webhook_secret) do
     {:error, "Webhook secret is required for this operation."}
   end
 
-  def valid_signature(_webhook_secret, body, _signature) when not is_bitstring(body) do
+  def validate_signature(_webhook_secret, body, _signature) when not is_bitstring(body) do
     {:error, "body should be passed as a string."}
   end
 
-  def valid_signature(_webhook_secret, _, signature) when not is_bitstring(signature) do
+  def validate_signature(_webhook_secret, _, signature) when not is_bitstring(signature) do
     {:error, "signature should be passed as a string."}
   end
 
-  def valid_signature(webhook_secret, body, signature) do
+  def validate_signature(webhook_secret, body, signature) do
     match? =
       :hmac
       |> :crypto.mac(:sha256, webhook_secret, body)
@@ -71,11 +71,11 @@ defmodule ExNylas.WebhookNotifications do
 
   ## Examples
 
-      iex> valid = ExNylas.WebhoookNotification.valid_signature!(webhook_secret, body, signature_from_webhook_request)
+      iex> valid = ExNylas.WebhoookNotification.validate_signature!(webhook_secret, body, signature_from_webhook_request)
   """
-  @spec valid_signature!(String.t(), String.t(), String.t()) :: boolean()
-  def valid_signature!(webhook_secret, body, signature) do
-    case valid_signature(webhook_secret, body, signature) do
+  @spec validate_signature!(String.t(), String.t(), String.t()) :: boolean()
+  def validate_signature!(webhook_secret, body, signature) do
+    case validate_signature(webhook_secret, body, signature) do
       {:ok, match?} ->
         match?
 
