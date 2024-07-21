2f846cee59e0658ca29da41510471e78fab1814e
nickbair <bairinc@gmail.com>
2024-07-21 13:25:43 -0600

    Add tests for create

---
+++ test/generated_tests/create_test.exs
@@ -0,0 +1,50 @@
+defmodule ExNylasTest.Create do
+  use ExUnit.Case, async: true
+  import ExNylasTest.Helper
+
+  @modules [
+    ApplicationRedirects,
+    Calendars,
+    Channels,
+    Connectors,
+    Contacts,
+    Drafts,
+    Events,
+    Folders,
+    Scheduling.Configurations,
+    Webhooks
+  ]
+
+  setup do
+    bypass = Bypass.open()
+    {:ok, bypass: bypass}
+  end
+
+  for module <- @modules do
+    @module module
+
+    generate_test(
+      @module,
+      :create,
+      [%{}],
+      %{
+        method: "POST",
+        response: ~s<{"data": {}}>,
+        assert_pattern: {:ok, %ExNylas.Response{data: %{}}}
+      }
+    )
+
+    generate_test(
+      @module,
+      :create!,
+      [%{}],
+      %{
+        method: "POST",
+        response: ~s<{"data": {}}>,
+        assert_pattern: %ExNylas.Response{data: %{}}
+      }
+    )
+  end
+
+  defp endpoint_url(port), do: "http://localhost:#{port}/"
+end
