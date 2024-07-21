8e079eb4c34d380e9f968a08bc64e1cdc6467e5c
nickbair <bairinc@gmail.com>
2024-07-21 13:21:11 -0600

    Add tests for delete

---
+++ test/generated_tests/delete_test.exs
@@ -0,0 +1,54 @@
+defmodule ExNylasTest.Delete do
+  use ExUnit.Case, async: true
+  import ExNylasTest.Helper
+
+  @modules [
+    ApplicationRedirects,
+    Calendars,
+    Channels,
+    Connectors,
+    Contacts,
+    Drafts,
+    Events,
+    Folders,
+    Grants,
+    MessageSchedules,
+    Messages,
+    Scheduling.Configurations,
+    Threads,
+    Webhooks
+  ]
+
+  setup do
+    bypass = Bypass.open()
+    {:ok, bypass: bypass}
+  end
+
+  for module <- @modules do
+    @module module
+
+    generate_test(
+      @module,
+      :delete,
+      ["id"],
+      %{
+        method: "DELETE",
+        response: ~s<{"data": {}}>,
+        assert_pattern: {:ok, %ExNylas.Response{data: %{}}}
+      }
+    )
+
+    generate_test(
+      @module,
+      :delete!,
+      ["id"],
+      %{
+        method: "DELETE",
+        response: ~s<{"data": {}}>,
+        assert_pattern: %ExNylas.Response{data: %{}}
+      }
+    )
+  end
+
+  defp endpoint_url(port), do: "http://localhost:#{port}/"
+end
