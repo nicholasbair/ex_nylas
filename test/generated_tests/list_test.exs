00d42700815ec5700908fb95b2e975b6fde1b96a
nickbair <bairinc@gmail.com>
2024-07-21 11:21:14 -0600

    Generate tests for list/list

---
+++ test/generated_tests/list_test.exs
@@ -0,0 +1,56 @@
+defmodule ExNylasTest.List do
+  use ExUnit.Case, async: true
+  import ExNylasTest.Helper
+
+  @modules [
+    Applications,
+    ApplicationRedirects,
+    Calendars,
+    Channels,
+    Connectors,
+    ContactGroups,
+    Contacts,
+    Drafts,
+    Events,
+    Folders,
+    Grants,
+    MessageSchedules,
+    Messages,
+    Scheduling.Configurations,
+    Threads,
+    Webhooks
+  ]
+
+  setup do
+    bypass = Bypass.open()
+    {:ok, bypass: bypass}
+  end
+
+  for module <- @modules do
+    @module module
+
+    generate_test(
+      @module,
+      :list,
+      [],
+      %{
+        method: "GET",
+        response: ~s<{"data": []}>,
+        assert_pattern: {:ok, %ExNylas.Response{data: []}}
+      }
+    )
+
+    generate_test(
+      @module,
+      :list!,
+      [],
+      %{
+        method: "GET",
+        response: ~s<{"data": []}>,
+        assert_pattern: %ExNylas.Response{data: []}
+      }
+    )
+  end
+
+  defp endpoint_url(port), do: "http://localhost:#{port}/"
+end
--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,1 +1,32 @@
 ExUnit.start()
+
+defmodule ExNylasTest.Helper do
+  use ExUnit.Case, async: true
+
+  defmacro generate_test(module, function, params, expected) do
+    quote do
+      test "#{unquote(module)}.#{unquote(function)} calls #{unquote(expected)[:method]}", %{bypass: bypass} do
+        Bypass.expect_once(bypass, fn conn ->
+          assert conn.method == unquote(expected)[:method]
+
+          conn
+          |> Plug.Conn.resp(200, unquote(expected)[:response])
+          |> Plug.Conn.put_resp_header("content-type", "application/json")
+        end)
+
+        params = unquote(params) ++ [
+          %ExNylas.Connection{
+            api_key: "1234",
+            grant_id: "1234",
+            api_server: endpoint_url(bypass.port),
+          }
+        ]
+
+        res = apply(Module.concat(ExNylas, unquote(module)), unquote(function), params)
+        pattern = unquote(expected)[:assert_pattern]
+
+        assert match?(pattern, res)
+      end
+    end
+  end
+end
