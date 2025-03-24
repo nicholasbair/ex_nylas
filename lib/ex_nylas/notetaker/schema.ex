f5733348f8d8e3bce9843036ea45982fca08451e
nickbair <bairinc@gmail.com>
2025-03-24 12:39:23 -0600

    Fixing schema, remove unneeded tests

--- lib/ex_nylas/notetaker/schema.ex
+++ lib/ex_nylas/notetaker/schema.ex
@@ -15,19 +15,21 @@ defmodule ExNylas.Notetaker do
     field(:join_time, :integer)
     field(:meeting_link, :string)
     field(:meeting_provider, Ecto.Enum, values: [:"Google Meet"])
-    field(:notetaker_state, Ecto.Enum, values: [:scheduled, :joining, :waiting_for_admission, :attending, :leaving, :done])
+    field(:status, Ecto.Enum, values: [:scheduled, :joining, :waiting_for_admission, :attending, :leaving, :concluded])
 
     embeds_one :meeting_settings, MeetingSettings, primary_key: false do
       field(:video_recording, :boolean)
       field(:audio_recording, :boolean)
       field(:transcription, :boolean)
+      field(:summary, :boolean)
+      field(:action_items, :boolean)
     end
   end
 
   @doc false
   def changeset(struct, params \\ %{}) do
     struct
-    |> cast(params, [:id, :notetaker_name, :join_time, :meeting_link, :meeting_provider, :notetaker_state])
+    |> cast(params, [:id, :notetaker_name, :join_time, :meeting_link, :meeting_provider, :status])
     |> cast_embed(:meeting_settings, with: &Util.embedded_changeset/2)
   end
 end
