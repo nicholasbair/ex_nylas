2a1f516f2e6f9352350b13c61c342b653bbe4eda
nickbair <nicholasbair@users.noreply.github.com>
2026-01-25 19:52:56 -0700

    fix: response handler errors

--- lib/ex_nylas/core/response_handler.ex
+++ lib/ex_nylas/core/response_handler.ex
@@ -16,15 +16,19 @@ defmodule ExNylas.ResponseHandler do
   def handle_response(res, transform_to \\ nil, use_common_response \\ true) do
     case format_response(res) do
       {:ok, body, status, headers} ->
-        {:ok, TF.transform(body, status, headers, transform_to, use_common_response, transform?(res))}
+        case TF.transform(body, status, headers, transform_to, use_common_response, transform?(res)) do
+          %DecodeError{} = error -> {:error, error}
+          result -> {:ok, result}
+        end
 
       {:error, body, status, headers} ->
         response = TF.transform(body, status, headers, transform_to, use_common_response, transform?(res))
 
         case response do
-          %Response{} -> {:error, APIError.exception(response)}
-          %{__struct__: _} = struct -> {:error, struct}  # Already an error struct (e.g., HostedAuthentication.Error)
-          other -> {:error, DecodeError.exception({:invalid_response_format, other})}
+          %Response{} = resp -> {:error, APIError.exception(resp)}
+          %DecodeError{} = error -> {:error, error}
+          %{__struct__: _} = struct -> {:error, struct}  # Other error structs (e.g., HostedAuthentication.Error)
+          non_json_response -> {:error, DecodeError.exception({:invalid_response_format, non_json_response})}
         end
 
       {:error, reason} when is_atom(reason) ->
